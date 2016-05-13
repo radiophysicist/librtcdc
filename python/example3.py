1bb292b16279288da89c38b35589a73bf28bca8c
Denis Shatov <dshatov@gmail.com>
2016-05-13 07:58:58 +0100

    Replace stun server in example 3 with ekiga's one

--- python/example3.py
+++ python/example3.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     # Если stun_server не задать, то падает при инициализации PeerConnection
     # C ошибкой corrupted double-linked list
     peer = pyrtcdc.PeerConnection(on_candidate=on_candidate,
-                                  stun_server='stun.services.mozilla.com')
+                                  stun_server='stun.ekiga.net')
     offer = peer.generate_offer()
     print("Exiting...")
